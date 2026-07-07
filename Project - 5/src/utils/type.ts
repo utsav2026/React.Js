9bc056600b068523942cb1fc26558b0566af2bd9
Utsav Baldaniya <butsav01@gmail.com>
2026-07-07 09:24:50 +0530

    Add files via upload

---
+++ Project - 5/src/utils/type.ts
@@ -0,0 +1,18 @@
+export interface productType {
+    product_name: string,
+    product_category: string,
+    product_price: number,
+    product_stock: number,
+    product_image: string,
+    product_description: string
+}
+
+export interface productTypeWithId {
+    id: string,
+    product_name: string,
+    product_category: string,
+    product_price: number,
+    product_stock: number,
+    product_image: string,
+    product_description: string
+}
